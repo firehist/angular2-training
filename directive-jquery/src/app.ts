ae8d1372233c2376f55a19d2189e34df992eabe2
Your Name <firehist@gmail.com>
2016-06-30 10:08:53 +0200

    Changes

---
+++ directive-jquery/src/app.ts
@@ -0,0 +1,17 @@
+import {Component} from '@angular/core'
+import { TypeaheadDirective } from './typeahead.component'
+
+@Component({
+  selector: 'app',
+  template: `
+    <h1>Directive jQuery typeahead</h1>
+    <p>We used this plugin: <a href="https://github.com/running-coder/jquery-typeahead" target="_blank">https://github.com/running-coder/jquery-typeahead</a></p>
+    <hr>
+    <input type="text" [(ngModel)]="myField" typeahead />
+  `,
+  directives: [ MyDatetimepickerDirective ]
+})
+export class App {
+  myField: string
+  constructor() {}
+}
